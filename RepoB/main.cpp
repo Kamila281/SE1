94c05ecb38574a06456a741214d0e426b0119122
Buliakova Kamila <k.buliakova@mail.ru>
2024-12-16 22:30:08 +0500

    Добавлена печать разности введенных чисел

---
+++ RepoB/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout<<"Введите первое целое число:";
+    cin>>a;
+    cout<<"Введите второе целое число:";
+    cin>>b;
+    cout<<"Вы ввели числа:"<<a<<"и"<<b<<endl;
+    int c=a-b;
+    cout<<"Разность чисел:"<<c<<endl;
+    return 0;
+}
