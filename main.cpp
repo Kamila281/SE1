15b519eb948c7d04e7f00f37df9cfd8f9d04e318
Buliakova Kamila <k.buliakova@mail.ru>
2024-12-16 16:07:04 +0500

    Добавили в программу ввод двух целых чисел

--- main.cpp
+++ main.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int main()
 {
+    int a, b;
+    cout<<"Введите первое целое число:";
+    cin>>a;
+    cout<<"Введите второе целое число:";
+    cin>>b;
+    cout<<"Вы ввели числа:"<<a<<"и"<<b<<endl;
+
     return 0;
 }
