42518463464240fcae489c348faac4dab2a9198f
Kamila281 <k.buliakova@mail.ru>
2024-12-18 17:34:35 +0500

    Add files via upload

---
+++ RepoA/main.cpp
@@ -0,0 +1,36 @@
+
+#Avtor: Buliakova Kamila
+#Group: KE-217
+#Year: 2024
+
+
+
+#avtor: Buliakova Kamila
+#group: KE-217
+#year: 2024
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int a, b;
+     cout << "Enter A and B: ";
+     cin >> a >> b;
+     cout << "A + B = " << a + b << '\n'
+          << "A - B = " << a - b << '\n'
+          << "A * B = " << a * b << '\n';
+
+    return 0;
+
+  int a, b;
+  cout << "Enter A and B: ";
+  cin >> a >> b;
+  cout << "A + B = " << a + b << '\n'
+       << "A - B = " << a - b << '\n'
+       << "A / B = " << a / b << '\n';
+  return 0;
+
+}
